4a92bd1b13f0c6d18ef1fe4673de334e529c1a05
LeVlad <leca.vlad@gmail.com>
2022-05-28 21:26:32 +0000

    Add 50% of CRUD

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+# creating a new model called Item
+
 class Item(models.Model):
+
+
     name = models.CharField(max_length=50, null=False, blank=False)
     priority = models.CharField(max_length=10, null=False, blank=False)
     location = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+# Overiding the standard return method of the models
+
+    def __str__(self):
+        return self.name
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item
 
 
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+def add_item(request):
+
+
+    if request.method == "POST":
+        name = request.POST.get("item_name")
+        location = request.POST.get("item_location")
+        priority = request.POST.get("item_priority")
+        done = "done" in request.POST
+        Item.objects.create(
+            name=name, location=location, priority=priority, done=done
+            )
+
+        return redirect('get_todo_list')
+
+    return render(request, 'todo/add_item.html')

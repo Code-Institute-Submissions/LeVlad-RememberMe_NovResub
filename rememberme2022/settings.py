759af360e2f4beaa61c3c6b1d9a102e817f795d7
LeVlad <leca.vlad@gmail.com>
2022-09-17 10:27:09 +0000

    Add env.py to gitignore

--- rememberme2022/settings.py
+++ rememberme2022/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,7 +54,7 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-GOOGLE_MAPS_API_KEY = 'AIzaSyDKLI-zDTvO4X-3nznxsmcXgw0pBTpruf0'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

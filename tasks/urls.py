d66331956cfba92f7f4460a82c3a7d3f34e62501
LeVlad <leca.vlad@gmail.com>
2022-10-25 22:02:07 +0000

    view error for task found and started fix

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'tasks'
 
 urlpatterns = [
+
     path('tasks', views.get_task_list, name='task_list'),
     path('tasks/add_task/', views.add_task, name='add'),
     path('edit/<task_id>/', views.edit_task, name='edit_task'),

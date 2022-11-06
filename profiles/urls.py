ca6974c79b1cd76def43e62224549fda4c744a78
LeVlad <leca.vlad@gmail.com>
2022-11-06 03:09:53 +0000

    Changed Debug=False

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from tasks import views as t_views
 from . import views
 
 
 urlpatterns = [
     path('', views.profiles_index, name='profiles'),
-    path('accounts/', include('allauth.urls')),
     path('tasks/', t_views.get_task_list, name='get_task_list'),
     path('tasks/add_task/', t_views.add_task, name='add_task'),
     path('tasks/edit/<int:task_id>/', t_views.edit_task, name='edit_task'),
